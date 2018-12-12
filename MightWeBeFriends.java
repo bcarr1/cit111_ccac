e3c2420169233038effabd232a2e0d6a9acede2b
bcarr1 <44411195+bcarr1@users.noreply.github.com>
2018-12-12 12:59:28 -0500

    Add files via upload

---
+++ MightWeBeFriends.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaObjects;
+
+/**
+ * "Blueprint" class for
+ * modeling a real life
+ * donut in Java
+ * @author bcarr1c
+ * 
+ */
+public class Donut { 
+    // member variables 
+    // that all Donut objects have
+    String donutType;
+    String name;
+    String texture;
+    int donutSize;
+    
+    // close class
+    
+}
